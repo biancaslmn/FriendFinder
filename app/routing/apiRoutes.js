87856a9440077310fe64f709f3409465b13e4689
Bianca Salomon <biancaslmn>
2018-11-27 22:05:10 -0500

    First push

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+// =============================================================
+// Create a set of routes that then display matches data as JSONs
+// =============================================================
+
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+  
+  app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
