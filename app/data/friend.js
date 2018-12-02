c78ab06f3f54e4cd38752f74aa1f5f9f04d7d28a
Bianca Salomon <biancaslmn>
2018-12-02 18:39:46 -0500

    second push

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with "test" data
+var friendList = [
+    {
+      name: 'Rami Malek',
+      profilePic: '../media/RamiMalek.jpg',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
